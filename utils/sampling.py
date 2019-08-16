d6f91d520ae4e3ad190d9bfd5ee2bae3583e8ae5
efeyusuf <yusufefe097@gmail.com>
2019-08-15 18:39:17 -0700

    mixed working

--- utils/sampling.py
+++ utils/sampling.py
@@ -8,7 +8,7 @@ from torchvision import datasets, transforms
 from utils.options import args_parser
 
 
-np.random.seed(42)  #to make results reproducible
+#np.random.seed(41)  #to make results reproducible
 args = args_parser()
 
 def mnist_iid(dataset, num_users):
@@ -124,24 +124,39 @@ def cifar_noniid(dataset, num_users):
         num_shards, num_imgs = 160 , 225
         idx_shard = [i for i in range(num_shards)]
         for i in range(10):
-            valid_set += list(idxs[num_tot/10*i:num_tot/10*i+500])
-            non_iid_set += list(idxs[num_tot/10*i+1400:num_tot/10*(i+1)])         
-            for j in range(args.mixed*num_users):
-                dict_users[j] += list(idxs[num_tot/10*i+500+45*j:num_tot/10*i+500+45*(j+1)])
-        for z in range(num_users*args.mixes, num_users):
+            valid_set = valid_set + list(idxs[(5000*i):5000*i+500])
+            non_iid_set = non_iid_set + list(idxs[(5000*i)+1400:5000*(i+1)])         
+            for j in range(int(args.mixed*num_users)):
+            #    dict_users[j] = dict_users[j] + list(idxs[(5000)*i+500+(45*j):(5000)*i+500+(45*(j+1))])
+                dict_users[j] = np.concatenate((dict_users[j], list(idxs[(5000)*i+500+(45*j):(5000)*i+500+(45*(j+1))])), axis=0)
+        for z in range(int(num_users*args.mixed), num_users):
             rand_set = set(np.random.choice(idx_shard, 2, replace=False))
             idx_shard = list(set(idx_shard) - rand_set)
             for rand in rand_set: 
-                dict_users[z] = np.concatenate((dict_users[z], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0) 
+                dict_users[z] = np.concatenate((dict_users[z], non_iid_set[rand*num_imgs:(rand+1)*num_imgs]), axis=0) 
+        for a in range(int(args.mixed*num_users)):
+            np.random.shuffle(dict_users[a])  
+        np.random.shuffle(valid_set)          
+        print("Labels for user 0:", labels[dict_users[0]])
+        print("Labels for user 19:", labels[dict_users[19]])
+        print("Labels for user 20:", labels[dict_users[20]])
+        print("Labels for user 90:", labels[dict_users[90]])        
         return dict_users,valid_set
 
                
 if __name__ == '__main__':
+    '''
     dataset_train = datasets.MNIST('../data/mnist/', train=True, download=True,
                                    transform=transforms.Compose([
                                        transforms.ToTensor(),
                                        transforms.Normalize((0.1307,), (0.3081,))
                                    ]))
+                                   
     num = 100
     d = mnist_noniid(dataset_train, num)
     print(d[100].shape)
+    '''
+    trans_cifar = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    dataset_train = datasets.CIFAR10('../data/cifar', train=True, download=True, transform=trans_cifar)
+    dict_users,valid_idxs = cifar_noniid(dataset_train, args.num_users)
+    print(dict_users[19].shape)
