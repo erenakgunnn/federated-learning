91b5c3c9d6f3f50d63ff10bf4875ea9007c4f38f
erenakgunnn <orhaneren32@hotmail.com>
2019-08-27 08:02:02 +0300

    fix

--- main_fed.py
+++ main_fed.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     # parse args
     args = args_parser()
     
-    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')'
+    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
     print(args.device)
     # load dataset and split users
     if args.dataset == 'mnist':
